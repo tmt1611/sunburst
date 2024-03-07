fec837a6a4b65efb64327625702955c26f621c4b
tmt1611 <82852830+tmt1611@users.noreply.github.com>
2024-03-07 16:49:40 +0100

    Update main.py

--- tmt1611.github.io/main.py
+++ tmt1611.github.io/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import datetime
+import plotly.subplots as sp 
 
 if __name__ == "__main__":
     url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTiwummYiOCTzagxsbho_ZAFxTpluojPuF6Ynaxjtato5r965ppSx0ST_XPQuwzxSpP_BcF51VMuprM/pub?output=xlsx'
@@ -8,24 +9,47 @@ if __name__ == "__main__":
     df = pd.read_excel(url)
     df = df.fillna(method='ffill', limit=1).fillna('')
 
-    fig = px.sunburst(
-        df,
-        names='ID',
-        parents='parent',
-        values='adjusted_popularity',
-        color='value_num',
-        hover_data='value',
-        branchvalues='total',
-        maxdepth = None
-    )
-    fig.update_layout(
-        margin = dict(t=10, l=10, r=10, b=10)
-    )
 
-    # fig.update_traces(insidetextorientation='radial')
+# Sunburst =======
+fig = px.sunburst(
+    df,
+    names='ID',
+    parents='parent',
+    values='adjusted_popularity',
+    color='value_num',
+    hover_data='value',
+    branchvalues='total',
+    maxdepth = None,
+)
 
-    fig.update_layout(
-    margin=dict(t=10, l=10, r=10, b=10),
+
+# Icicle =========
+fig2 = px.icicle(
+    df,
+    names='ID',
+    parents='parent',
+    values='adjusted_popularity',
+    color='value_num',
+    hover_data='value',
+    branchvalues='total',
+    maxdepth = None,
+)
+
+fig2.update_traces(tiling = dict(
+            orientation='v'
+))
+
+
+# Create subplots =========
+fig3 = sp.make_subplots(rows=1, cols=2, specs=[[{'type': 'domain'}, {'type': 'domain'}]])
+
+# Add traces to subplots
+fig3.add_trace(fig.data[0], row=1, col=1)
+fig3.add_trace(fig2.data[0], row=1, col=2)
+
+# Update layout
+fig3.update_layout(
+    margin=dict(t=30, l=10, r=10, b=10),
     annotations=[
         dict(
             x=0,
@@ -44,4 +68,4 @@ if __name__ == "__main__":
     ]
 )
 
-    fig.write_html("index.html", include_plotlyjs="cdn")
+fig3.write_html("index.html", include_plotlyjs="cdn")
